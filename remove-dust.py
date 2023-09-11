13f0807323b7d17de37d94ffe28fd49dfaaf4b83
Max Lansing <lansing@gmail.com>
2023-09-10 19:15:12 -0700

    feature: add gamma adustment, biharmonic algorithm (skimage), subfolder support

--- remove-dust.py
+++ remove-dust.py
@@ -4,13 +4,19 @@ import os
 import cv2
 import numpy as np
 import rawpy
+from pyinpaint import Inpaint
 from pyunraw import PyUnraw
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input', help='Silverfast 64 bit DNG file')
 parser.add_argument('-c', '--max-coverage', type=float, default=0.002)
 parser.add_argument('-b', '--border-buffer', type=float, default=5.0)
+parser.add_argument('-g', '--gamma', type=float, default=2.2)
 parser.add_argument('-m', '--write-mask', action='store_true')
+parser.add_argument('-f', '--subfolder', type=str)
+parser.add_argument('-a', '--algorithm', type=str, default='telea')
+
 
 args = parser.parse_args()
 
@@ -18,9 +24,9 @@ input_path = args.input
 
 BORDER_BUFFER = args.border_buffer
 INPAINT_RADIUS = 0.95
-INPAINT_ALGO = cv2.INPAINT_TELEA
 MAX_COVERAGE = args.max_coverage
 MAX_COVERAGE_JUMP = 1.8 # not currently used
+
 
 # Write TIFF w IR channel
 ir_path = input_path + '.ir.tiff'
@@ -50,20 +56,45 @@ for threshold in range(50, 90):
     # mask_path = f"{ir_path}.mask.{threshold}.tiff"
     # cv2.imwrite(mask_path, thresh_mask)
 
-print(f"threshold: {threshold} coverage: {mask_coverage}")
+print(f"threshold: {threshold-1} coverage: {prev_mask_coverage}")
 
 if args.write_mask:
     mask_path = f"{ir_path}.mask.{threshold}.tiff"
     cv2.imwrite(mask_path, thresh_mask)
 
+def adjust_gamma(image, gamma=1.0):
+    inv_gamma = 1.0 / gamma
+    table = ((np.arange(0, 65536) / 65535) ** inv_gamma) * 65535
+    table = table.astype(np.uint16)
+    return table[image]
+
 cleaned_rgb_img = np.zeros(rgb_img.shape).astype(np.uint16)
 
-for channel in range(3):
-    channel_img = rgb_img[:,:,channel]
-    cleaned_channel = cv2.inpaint(channel_img, thresh_mask, INPAINT_RADIUS, INPAINT_ALGO)
-    cleaned_rgb_img[:,:,channel] = cleaned_channel
+if args.algorithm == 'telea':
+    INPAINT_ALGO = cv2.INPAINT_TELEA
+    for channel in range(3):
+        channel_img = rgb_img[:,:,channel]
+        cleaned_channel = cv2.inpaint(channel_img, thresh_mask, INPAINT_RADIUS, INPAINT_ALGO)
+        cleaned_rgb_img[:,:,channel] = cleaned_channel
+elif args.algorithm == 'biharmonic':
+    from skimage.restoration import inpaint
+    cleaned_rgb_img_float = inpaint.inpaint_biharmonic(rgb_img, thresh_mask, channel_axis=-1)
+    cleaned_rgb_img = (cleaned_rgb_img_float * 65535).astype(np.uint16)
 
-cleaned_path = input_path + ".cleaned.tiff"
-cv2.imwrite(cleaned_path, cleaned_rgb_img)
+if args.gamma != 1.0:
+    for channel in range(3):
+        cleaned_rgb_img[:,:,channel] = adjust_gamma(cleaned_rgb_img[:,:,channel], args.gamma)
+
+if args.subfolder:
+    output_dir = os.path.join(os.path.dirname(input_path), args.subfolder)
+    if not os.path.exist(output_dir):
+        os.mkdir(output_dir)
+else:
+    output_dir = os.path.dirname(input_path)
+
+output_basename = os.path.splitext(os.path.basename(input_path))[0]
+output_path = os.path.join(output_dir, output_basename + "-cleaned.tiff")
+
+cv2.imwrite(output_path, cleaned_rgb_img, params=(cv2.IMWRITE_TIFF_COMPRESSION, 32946))
 
 os.remove(ir_path)
